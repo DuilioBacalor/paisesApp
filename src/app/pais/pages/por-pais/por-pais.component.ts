c2a11cb5ad070b5d9bbcdae2b910a21c366e6c2d
Duilio <duiliobacalorwork>
2022-07-27 14:54:47 -0300

    paisesConErrorRouterLink

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -10,12 +11,16 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent  {
 termino:string="";
 hayError:boolean=false;
+paises:country[]=[];
   constructor(private paisService:PaisService) { }
   buscar(){
     this.hayError=false;
     this.paisService.buscarPais(this.termino).subscribe((paises) =>{
+      this.paises=paises;
       console.log(paises);
-    },(err)=>{this.hayError=true;});
+    },(err)=>{this.hayError=true;
+    this.paises=[];
+    });
     // console.log(this.termino);
   }
 
